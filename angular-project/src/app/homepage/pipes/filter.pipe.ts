ed9a838914996fe9c9e074d9cf600223b427b39b
timotejbalajka@gmail.com <timotejbalajka@gmail.com>
2024-05-28 08:05:09 +0200

    FilterPipe ide

--- angular-project/src/app/homepage/pipes/filter.pipe.ts
+++ angular-project/src/app/homepage/pipes/filter.pipe.ts
@@ -8,28 +8,26 @@ import { Shoes } from '../../interfaces/shoes';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: Shoes[], filterParameters: WritableSignal<FilterParameter>): any[] {
-
-   /* const hasActiveValues = Object.values(filterParameters.value).some((filterValues) => filterValues.size > 0);*/
+  transform(value: Shoes[], filterParameters: FilterParameter): any[] {
 
     if (!value) {
       return null;
     }
 
-    if (Object.values(filterParameters()).every(filterValues => filterValues.size === 0)
+    if (Object.values(filterParameters).every(filterValues => filterValues.size === 0)
 )   {
       return value;
     }
 
     return value.filter((item: Shoes) => {
-      console.log(filterParameters());
-      const priceMatch = filterParameters().price.has(item.price);
-      const nameMatch = filterParameters().name.has(item.name.toLowerCase());
-      const brandMatch = filterParameters().brand.has(item.shoeBrand.toLowerCase());
-      const sizeMatch = filterParameters().size.has(item.shoeSize);
-      const colorMatch = filterParameters().color.has(item.shoeColor.toLowerCase());
-      const materialMatch = filterParameters().material.has(item.shoeMaterial.toLowerCase());
-      const ratingMatch = filterParameters().rating.has(item.rating);
+      console.log(filterParameters);
+      const priceMatch = filterParameters.price.has(item.price);
+      const nameMatch = filterParameters.name.has(item.name);
+      const brandMatch = filterParameters.brand.has(item.shoeBrand);
+      const sizeMatch = filterParameters.size.has(item.shoeSize);
+      const colorMatch = filterParameters.color.has(item.shoeColor);
+      const materialMatch = filterParameters.material.has(item.shoeMaterial);
+      const ratingMatch = filterParameters.rating.has(item.rating);
 
       return priceMatch || nameMatch || brandMatch || sizeMatch || colorMatch || materialMatch || ratingMatch;
     });
